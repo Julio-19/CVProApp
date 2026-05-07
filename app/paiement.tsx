24afc3196f607ffb13d189ed6152425a67e2d9d8
Julio_19 <sounoujulio@gmail.com>
2026-05-07 01:13:52 +0100

    Auto-save 2026-05-07 01:13:52

--- app/paiement.tsx
+++ app/paiement.tsx
@@ -279,7 +279,7 @@ export default function PaiementScreen() {
                 </View>
               </View>
 
-              <Text style={styles.sectionTitle}>Mode de paiement</Text>
+              {/* <Text style={styles.sectionTitle}>Mode de paiement</Text> */}
 
               {[
                 {
