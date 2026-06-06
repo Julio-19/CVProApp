1c26d695030c651848d5fad9ccf5188bd89ffe6b
Julio_19 <sounoujulio@gmail.com>
2026-06-06 11:28:56 +0100

    Auto-save 2026-06-06 11:28:56

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Configuration critique pour le web
+config.resolver.assetExts.push('cjs');
+
+config.transformer.getTransformOptions = () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
+
+module.exports = config;
