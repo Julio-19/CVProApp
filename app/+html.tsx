1562237ac4cc7a2cf10ce1efb982899e8dc449b6
Julio_19 <sounoujulio@gmail.com>
2026-06-11 13:35:52 +0100

    Auto-save 2026-06-11 13:35:52

---
+++ app/+html.tsx
@@ -0,0 +1,19 @@
+// app/+html.tsx
+import { type PropsWithChildren } from 'react';
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="fr">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
