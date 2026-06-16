b99d2f4251e87d27c923afd76a13d3db8bdfc53c
Julio_19 <sounoujulio@gmail.com>
2026-06-16 14:48:30 +0100

    Auto-save 2026-06-16 14:48:30

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,4 +1,5 @@
 import { type PropsWithChildren } from 'react';
+import { ScrollViewStyleReset } from 'expo-router/html';
 
 export default function Root({ children }: PropsWithChildren) {
   return (
@@ -7,6 +8,27 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        
+        {/* IMPORTANT: Base URL pour GitHub Pages */}
+        <base href="/CVProApp/" />
+        
+        {/* Reset des styles pour expo-router */}
+        <ScrollViewStyleReset />
+        
+        {/* Styles de base */}
+        <style dangerouslySetInnerHTML={{ __html: `
+          body {
+            margin: 0;
+            padding: 0;
+            min-height: 100vh;
+            background-color: #ffffff;
+          }
+          #root {
+            min-height: 100vh;
+          }
+        ` }} />
+        
+        {/* Scripts externes */}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" />
       </head>
       <body>{children}</body>
