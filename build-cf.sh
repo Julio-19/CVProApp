bcf71df5b248adabefd3ffebdd942fc96e2e2c20
Julio_19 <sounoujulio@gmail.com>
2026-06-27 10:57:19 +0100

    Auto-save 2026-06-27 10:57:19

--- build-cf.sh
+++ build-cf.sh
@@ -5,4 +5,5 @@ npx expo export --platform web
 sed -i 's|<script src="/_expo|<script type="module" src="/_expo|g' dist/index.html
 sed -i 's|</head>|<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script></head>|g' dist/index.html
 cp dist/index.html dist/404.html
+cp public/_redirects dist/_redirects
 echo "Build termine!"
