e215a833a05117519783fb1ca96325c2efcf6f79
Julio_19 <sounoujulio@gmail.com>
2026-06-29 03:35:33 +0100

    Auto-save 2026-06-29 03:35:33

--- app/templates.tsx
+++ app/templates.tsx
@@ -199,7 +199,7 @@ export default function TemplatesScreen() {
   const [filtre, setFiltre]     = useState<'tous' | 'gratuits' | 'achetes'>('tous');
   const [achetes, setAchetes]   = useState<string[]>([]);
   const [loadingAchetes, setLoadingAchetes] = useState(true);
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  
 
   useEffect(() => {
     chargerAchetes();
